7efc8b0ab067881f6ba0b433dd02ecec91cd8c14
João Paulo <jp.sport17@gmail.com>
2024-03-29 16:02:00 -0300

    feat: Adicionado HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_elevated_button/gradient_elevated_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +9,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
     });
   }
 
@@ -20,7 +19,47 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Spacer(flex: 4,),
+            const Flexible(
+                flex: 2,
+                child: Text('Olá, bem-vindo(a)', style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),)),
+            const Spacer(),
+            const Flexible(
+                flex: 5,
+                child: Image(image: AssetImage('assets/images/Logo_MedFacil.png')),),
+            const Spacer(),
+            Flexible(
+              flex: 5,
+              child: GradientElevatedButton(
+              onPressed: () {
+                //TODO: Implementar Login
+              },
+              style: GradientElevatedButton.styleFrom(
+                gradient: const LinearGradient(colors: [
+                  Color(0x623ACEB5),
+                  Color(0x626052FF)
+                ],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+
+              ),
+              child: const Text("Login", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 20)),
+            )),
+            const Spacer(),
+            Flexible(
+              flex: 14,
+              child: TextButton(onPressed: (){
+
+              }, child: const Text('Cadastrar', style: TextStyle(decoration: TextDecoration.underline, fontSize: 20, fontWeight: FontWeight.w600),)))
+          ],
+        ),
+      ),
     );
   }
 }
